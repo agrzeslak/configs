7420fee30fcd66cdac4e62de50048e38dcf48b3c
root <root@localhost.localdomain>
2019-02-08 12:20:38 +1100

    Refactored script to make it easier to add new entries

--- setup.sh
+++ setup.sh
@@ -1,30 +1,61 @@
-#!/bin/sh
+#!/bin/bash
+apt_install=(
+    "gobuster"
+)
+git_repos_opt=(
+    "https://github.com/jpillora/chisel.git"  # TCP tunnel over HTTP
+    "https://github.com/anshumanbh/git-all-secrets.git"  # Git repo scanner (passwords, keys, etc.)
+    "https://github.com/SecureAuthCorp/impacket.git"  # Python network scripts
+    "https://github.com/rebootuser/LinEnum.git"  # Local Linux enumeration
+    "https://github.com/GerbenJavado/LinkFinder.git"  # Finds links in JS files
+    "https://github.com/dxa4481/truffleHog.git"  # Git repo scanner (passwords, keys, etc.)
+)
+git_repos_usr_share=(
+    "https://github.com/danielmiessler/SecLists.git"  # Wordlists etc.
+)
+
 # Copy configs to required locations
 cp .tmux.conf ~/ && tmux source-file ~/.tmux.conf
 cp .vimrc ~/
 
 if [ "$1" != "--configs-only" ]; then
-    # Clone repos which aren't included in Kali
-    cd /usr/share
-    if (git clone https://github.com/danielmiessler/SecLists.git); then seclists=true; else seclists=false; fi  # Wordlists etc.
+    results="\nApt install results:\n"
+    for package in ${apt_install[*]}; do
+        if (apt install $package); then
+            results="${results} \033[0;32m$package - Success\033[0m\n"
+        else
+            results="${results} \033[0;31m$package - Failure\033[0m\n"
+        fi
+    done
 
+    results="${results}\nGit clone results:\n"
     cd /opt
-    if (git clone https://github.com/jpillora/chisel.git); then chisel=true; else chisel=false; fi  # TCP tunnel over HTTP
-    if (git clone https://github.com/SecureAuthCorp/impacket.git); then impacket=true; else impacket=false; fi  # Python network scripts
-    if (git clone https://github.com/rebootuser/LinEnum.git); then linenum=true; else linenum=false; fi  # Local Linux enumeration
+    for repo in ${git_repos_opt[*]}; do
+        dir="/opt/$(echo $repo | awk -F'/' '{print $5}' | awk -F'.' '{print $1}')"
+        if [ -d "$dir" ]; then
+                results="${results} \033[0;34m$dir - Directory already exists\033[0m\n"
+        else
+            if (git clone $repo); then
+                results="${results} \033[0;32m$dir - Success\033[0m\n"
+            else
+                results="${results} \033[0;31m$dir - Failure\033[0m\n"
+            fi
+        fi
+    done
 
-    # Apt install non-standard packages
-    if (apt install gobuster); then gobuster=true; else gobuster=false; fi
-
-    # Print git clone results
-    echo "\nGit clone results:"
-    if $seclists; then echo "\033[0;32mSecLists - Success\033[0m"; else echo "\033[0;31mSecLists - Failure\033[0m"; fi
-    if $chisel; then echo "\033[0;32mChisel - Success\033[0m"; else echo "\033[0;31mChisel - Failure\033[0m"; fi
-    if $impacket; then echo "\033[0;32mImpacket - Success\033[0m"; else echo "\033[0;31mImpacket - Failure\033[0m"; fi
-    if $linenum; then echo "\033[0;32mLinEnum - Success\033[0m"; else echo "\033[0;31mLinEnum - Failure\033[0m"; fi
-
-    # Print apt install results
-    echo "\nApt install results:"
-    if $gobuster; then echo "\033[0;32mGobuster - Success\033[0m"; else echo "\033[0;31mGobuster - Failure\033[0m"; fi
+    cd /usr/share
+    for repo in ${git_repos_usr_share[*]}; do
+        dir="/usr/share/$(echo $repo | awk -F'/' '{print $5}' | awk -F'.' '{print $1}')"
+        if [ -d "$dir" ]; then
+                results="${results} \033[0;34m$dir - Directory already exists\033[0m\n"
+        else
+            if (git clone $repo); then
+                results="${results} \033[0;32m$dir - Success\033[0m\n"
+            else
+                results="${results} \033[0;31m$dir - Failure\033[0m\n"
+            fi
+        fi
+    done
 fi
 
+echo -e $results
