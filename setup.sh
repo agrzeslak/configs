b25919f8e9261939382307ac7bc318f509fa3ab1
Andrzej Grzeslak <agrzeslak0@gmail.com>
2019-07-24 22:03:57 +1000

    Updated to save git repos at /usr/share, rather than at /opt and added empire and watson

--- setup.sh
+++ setup.sh
@@ -6,20 +6,20 @@ apt_install=(
     "lftp"
     "ncat"
 )
-git_repos_opt=(
+git_repos=(
     "https://github.com/jpillora/chisel.git"  # TCP tunnel over HTTP
+    "https://github.com/EmpireProject/Empire"  # Windows post-exploitation
     "https://github.com/anshumanbh/git-all-secrets.git"  # Git repo scanner (passwords, keys, etc.)
     "https://github.com/rebootuser/LinEnum.git"  # Local Linux enumeration
     "https://github.com/GerbenJavado/LinkFinder.git"  # Finds links in JS files
     "https://github.com/DominicBreuker/pspy.git"  # Process Spy
     "https://github.com/epi052/recursive-gobuster.git"  # Recursive wrapper around gobuster
+    "https://github.com/PowerShellMafia/PowerSploit.git"  # Powershell Scripts
+    "https://github.com/danielmiessler/SecLists.git"  # Wordlists etc.
     "https://github.com/0x00-0x00/ShellPop.git"  # Shell generator
     "https://github.com/tmux-plugins/tmux-logging.git"  # tmux logging plugin
     "https://github.com/dxa4481/truffleHog.git"  # Git repo scanner (passwords, keys, etc.)
-)
-git_repos_usr_share=(
-    "https://github.com/danielmiessler/SecLists.git"  # Wordlists etc.
-    "https://github.com/PowerShellMafia/PowerSploit.git"  # Powershell Scripts
+    "https://github.com/rasta-mouse/Watson"  # Missing KB enumeration
 )
 
 # Copy configs to required locations
@@ -37,23 +37,8 @@ if [ "$1" != "--configs-only" ]; then
         fi
     done
 
-    results="${results}\nGit clone results:\n"
-    cd /opt
-    for repo in ${git_repos_opt[*]}; do
-        dir="/opt/$(echo $repo | awk -F'/' '{print $5}' | awk -F'.' '{print $1}')"
-        if [ -d "$dir" ]; then
-            results="${results} \033[0;34mDirectory already exists\033[0m - $dir ($repo)\n"
-        else
-            if (git clone $repo); then
-                results="${results} \033[0;32mSuccess\033[0m - $dir ($repo)\n"
-            else
-                results="${results} \033[0;31mFailure\033[0m - $dir ($repo)\n"
-            fi
-        fi
-    done
-
     cd /usr/share
-    for repo in ${git_repos_usr_share[*]}; do
+    for repo in ${git_repos[*]}; do
         dir="/usr/share/$(echo $repo | awk -F'/' '{print $5}' | awk -F'.' '{print $1}')"
         if [ -d "$dir" ]; then
             results="${results} \033[0;34mDirectory already exists\033[0m - $dir ($repo)\n"
