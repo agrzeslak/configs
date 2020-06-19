70f2c5b16580528cfb8fd6d065864e3abe1c0ac3
Andrzej Grzeslak <agrzeslak0@gmail.com>
2020-06-19 11:13:24 +1000

    Increased wait for snapd.socket to be available

--- setup.sh
+++ setup.sh
@@ -81,7 +81,7 @@ if [ "$1" != "--configs-only" ]; then
 
     # p3x-onenote
     sudo systemctl enable --now snapd.socket
-    sleep 10  # wait for socket to become available
+    sleep 20  # wait for socket to become available
     sudo snap install p3x-onenote
     systemctl enable apparmor
 
