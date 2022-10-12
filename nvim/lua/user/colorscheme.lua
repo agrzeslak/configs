1c3fd9c445fdb2b5bb519e64f6575cf93a4890ff
Andrzej Grzeslak <agrzeslak0@gmail.com>
2022-10-12 16:00:21 +1100

    Keymaps: right-click pop-up menu LSP functions.

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -9,6 +9,6 @@ vim.g.gruvbox_color_column = "dark0"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
+    vim.notify("colorscheme " .. colorscheme .. " not found!")
+    return
 end
--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -12,5 +12,19 @@ vim.g.maplocalleader = " "
 
 -- Telescope
 keymap("n", "<C-p>", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_ivy({}))<cr>", opts)
-keymap("n", "<leader>s", "<cmd>lua require'telescope.builtin'.live_grep(require('telescope.themes').get_ivy({}))<cr>", opts)
+keymap("n", "<leader>s", "<cmd>lua require'telescope.builtin'.live_grep(require('telescope.themes').get_ivy({}))<cr>",
+    opts)
 keymap("n", "<leader>;", "<cmd>lua require'telescope.builtin'.buffers(require('telescope.themes').get_ivy({}))<cr>", opts)
+
+-- PopUp menu (right-click)
+vim.cmd([[
+    aunmenu PopUp
+    anoremenu PopUp.Go\ To\ Definition          <cmd>lua require'telescope.builtin'.lsp_definitions(require('telescope.themes').get_ivy({}))<cr>
+    anoremenu PopUp.Go\ To\ References          <cmd>lua require'telescope.builtin'.lsp_references(require('telescope.themes').get_ivy({}))<cr>
+    anoremenu PopUp.Go\ To\ Declaration         <cmd>lua vim.lsp.buf.declaration()<CR>
+    anoremenu PopUp.Go\ To\ Implementations     <cmd>lua require'telescope.builtin'.lsp_implementations(require('telescope.themes').get_ivy({}))<cr>
+    anoremenu PopUp.Go\ To\ Type\ Definitions   <cmd>lua require'telescope.builtin'.lsp_type_definitions(require('telescope.themes').get_ivy({}))<cr>
+    anoremenu PopUp.Hover                       <cmd>lua vim.lsp.buf.hover()<CR>
+    anoremenu PopUp.Signature\ Help             <cmd>lua vim.lsp.buf.signature_help()<CR>
+    anoremenu PopUp.Rename                      <cmd>lua vim.lsp.buf.rename()<CR>  
+]])
